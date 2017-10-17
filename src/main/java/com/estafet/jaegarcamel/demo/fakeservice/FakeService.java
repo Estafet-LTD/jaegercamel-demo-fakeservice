2c6517934650e3b93803fc3ee096e0c7f0df2d88
Ivo Costa <ivo.costa@estafet.com>
2017-10-17 12:11:17 +0100

    missing annotation

--- src/main/java/com/estafet/jaegarcamel/demo/fakeservice/FakeService.java
+++ src/main/java/com/estafet/jaegarcamel/demo/fakeservice/FakeService.java
@@ -1,10 +1,12 @@
 package com.estafet.jaegarcamel.demo.fakeservice;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by ivo.costa@estafet.com on 17/10/2017.
  */
+@Component
 public class FakeService extends RouteBuilder {
     @Override
     public void configure() throws Exception {
